df9c3112911d5e230bfc3eb83f1f8842b2cbb3e9
Caitlin Potter <caitpotter88@gmail.com>
2014-08-15 22:06:22 -0400

    feat(calendar): allow formatting local date + time
    Also changed to call Moment#local() before formatting, just to be explicit.

--- src/calendar.js
+++ src/calendar.js
@@ -1,8 +1,18 @@
 var moment = require('moment');
 
+// The GitHub API appears to serve dates in the timezone of the requester, so there should be
+// little reason to manipulate these further. However, they do provide timezone information should
+// the need arise in the future.
 function GHC$$calendar$$short(date) {
   date = moment(date);
-  return date.format('YYYY/MM/DD');
+  return date.local().format('YYYY/MM/DD');
+}
+
+// TODO(@caitp): switch between 12hour/24hour displays based on preferences.
+function GHC$$calendar$$shortWithTime(date) {
+  date = moment(date);
+  return date.local().format('YYYY/MM/DD hh:mm a');
 }
 
 exports.short = GHC$$calendar$$short;
+exports.shortWithTime = GHC$$calendar$$shortWithTime;
