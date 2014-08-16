7d9d53e5148197280ecbb1d382a7659c47c6dfbc
Caitlin Potter <caitpotter88@gmail.com>
2014-08-15 22:07:09 -0400

    feat(commits): add long formatter for commits command
    The long formatter outputs the following:
    
    ```
    Commit: <sha>
    Author: {{author login}} ({{author git-config name}}) <{{author email}}>
    Committer: {{committer login}} ({{commiter git-config name}}) <{{committer email}}>
    Authored: {{date of patch}}
    Checked in: {{date of checkin}}
    
    {{Commit Message}}
    ```
    
    There's very little text formatting for the long display currently, but it would be nice to
    align each field so that they read easily
    
    Closes #1
    Closes #2

--- src/commands/commits.js
+++ src/commands/commits.js
@@ -4,6 +4,20 @@ var calendar = require('../calendar');
 var trim = require('../trim');
 var pad = require('../pad');
 
+function fullName(login, extra) {
+  var name = (extra && extra.name) || '';
+  var email = (extra && extra.email) || '';
+  var result = login || '';
+
+  if (name && name !== login) {
+    result += (' (' + name + ')');
+  }
+  if (email) {
+    result += ' <' + email + '>';
+  }
+  return result;
+}
+
 function GHC$$commits$$oneline(commit) {
   var c = commit.commit;
   var author = pad.left((commit.author && commit.author.login) || '', 19) + ' ';
@@ -14,8 +28,26 @@ function GHC$$commits$$oneline(commit) {
   return sha + author + msg + date;
 }
 
-function GHC$$commit$$details(commit) {
-  
+function GHC$$commits$$details(commit) {
+  // Roughly the equivalent of git log --pretty=medium --- todo support more format modes
+  var c = commit.commit;
+  var sha = commit.sha || '';
+  var author = fullName(commit.author.login, c.author) || '?';
+  var committer = fullName(commit.committer.login, c.committer) || '?';
+  var adate = calendar.shortWithTime(commit.author.date) || '?';
+  var cdate = calendar.shortWithTime(commit.committer.date) || '?';
+  var message = c.message || '';
+
+  return [
+    'Commit: ' + sha,
+    'Author: ' + author,
+    'Committer: ' + committer,
+    'Authored: ' + adate,
+    'Checked in: ' + cdate,
+    '',
+    message,
+    '',
+  ].join('\n');
 }
 
 function GHC$$commits(options) {
