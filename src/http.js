0ff4cacab8068599516eeb9969905039a900e265
Caitlin Potter <caitpotter88@gmail.com>
2014-08-17 18:08:05 -0400

    feat(http): enable building of query parameters
    Because of this, the `--author` option now actually works, and this is a good thing.
    
    Closes #10
    Closes #11

--- src/http.js
+++ src/http.js
@@ -4,9 +4,122 @@ var _https;
 try { _https = require('https'); } catch (e) {};
 var url = require('url');
 var log = require('./log');
+var moment = require('./third_party/moment');
 
-function params(options, request) {
-  return '';
+var ARRAY_BRACKET_REGEXP = /\[\]$/;
+var INT_REGEXP = /^\d+$/;
+
+function buildURLParams(key, value, add) {
+  var i, ii, name, v, classicArray = false;
+  if (Array.isArray(value)) {
+    classicArray = key.test(ARRAY_BRACKET_REGEXP);
+    for (i=0, ii=value.length; i<ii; ++i) {
+      v = value[i];
+      if (classicArray) {
+        add(key, v);
+      } else {
+        buildURLParams(key + '[' + (typeof v === 'object' ? i : '') + ']', v, add);
+      }
+    }
+  } else if (typeof value === 'object') {
+    Object.keys(value).forEach(function(name) {
+      buildURLParams(key + '[' + name + ']', value[name], add);
+    });
+  } else {
+    add(key, value);
+  }
+}
+
+// Based on jQuery.param (2.x.x)
+function serializeURLParams(params) {
+  var key, s;
+  function add(key, value) {
+    value = typeof value === 'function' ? value() : value == null ? '' : value;
+    s[s.length] = encodeURIComponent(key) + '=' + encodeURIComponent(value);
+  }
+
+  if (typeof params === 'object') {
+    s = [];
+    for (key in params) {
+      if (params.hasOwnProperty(key)) {
+        buildURLParams(key, params[key], add);
+      }
+    }
+  }
+
+  return s.join('&').replace(/%20/, '+');
+}
+
+var FILTER_KEYS = {
+  'author': true
+};
+
+var FILTER_TYPES = {
+  'author': 'string'
+};
+
+function params(options) {
+  var params;
+  var key;
+  var value;
+  var result = '';
+  if (!options || typeof options !== "object") {
+    return '';
+  }
+
+  params = {};
+
+  Object.keys(options).forEach(function(key) {
+    value = FILTER_KEYS[key];
+    if (value === true) {
+      params[key] = options[key];
+    } else if (typeof value === 'string') {
+      params[value] = options[key];
+    }
+  });
+
+  Object.keys(params).forEach(function(key) {
+    value = params[key];
+    var type = FILTER_TYPES[key];
+
+    if (typeof value === 'function') {
+      value = value();
+    }
+
+    switch (type) {
+      case 'date':
+        if (typeof value === 'number' || typeof value === 'string' || typeof value === 'object') {
+          value = moment(value);
+          if (value.isValid()) {
+            // TODO(@caitp): does GitHub like dates formatted this way?
+            params[key] = value.format();
+            break;
+          }
+        }
+        delete params[key];
+        break;
+      case 'int':
+        if ((typeof value === 'string' && INT_REGEXP.test(value)) || typeof value === 'number') {
+          value = Number(value);
+          if (value === value && Math.abs(value) !== Infinity) {
+            params[key] = value | 0; // cast to int
+            break;
+          }
+        }
+        delete params[key];
+        break;
+      case 'string':
+        if (typeof params[key] !== 'string') {
+          delete params[key];
+        }
+        break;
+      default:
+        delete params[key];
+    }
+  });
+
+  result = serializeURLParams(params);
+  return result ? '?' + result : '';
 }
 
 function formatUrl(template, args) {
@@ -44,7 +157,8 @@ function buildUrl(options, request) {
   if (typeof request.command === 'string') {
     parts.push(request.command);
   }
-  return formatUrl('%@/%@%@', options.baseUrl, parts.join('/'), params(options, request));
+
+  return formatUrl('%@/%@%@', options.baseUrl, parts.join('/'), params(options));
 }
 
 function tryParseJSON(data) {
@@ -67,6 +181,8 @@ function GHC$$httpRequest(options, request) {
   return new Promise(function(resolve, reject) {
     var origUrl;
     var url = require('url').parse(origUrl = buildUrl(options, request));
+
+    log.verbose(options, 'Requesting:%green%', origUrl);
 
     url.method = request.method || 'get';
 
