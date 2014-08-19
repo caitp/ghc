47ba83061d81597cbf550b8246b2057a4341f3c5
Caitlin Potter <caitpotter88@gmail.com>
2014-08-18 22:36:06 -0400

    feat(http): allow filtering --since and --until dates
    This is still a bit flakey, and doesn't really parse dates that are entered manually very nicely
    yet. This will be easier once tests are written. Works well enough for now though.
    
    One of the difficulties is that github always serves up the most recent commits for a given range,
    so a bit of extra specificity is needed

--- src/calendar.js
+++ src/calendar.js
@@ -14,5 +14,38 @@ function GHC$$calendar$$shortWithTime(date) {
   return date.local().format('YYYY/MM/DD hh:mm a');
 }
 
+function GHC$$calendar$$now() {
+  return moment().local().toISOString();
+}
+
+function GHC$$calendar$$today() {
+  return moment().hours(0).minutes(0).seconds(0).local().toISOString();
+}
+
+function GHC$$calendar$$yesterday() {
+  return moment().hours(0).minutes(0).seconds(0).day(moment().day()-1).local().
+    toISOString();
+}
+
+function GHC$$calendar$$lastWeek() {
+  return moment().hours(0).minutes(0).seconds(0).day(-6).local().toISOString();
+}
+
+function GHC$$calendar$$lastMonth() {
+  return moment().hours(0).minutes(0).seconds(0).month(moment.month()-1).day(0).local().
+    toISOString();  
+}
+
+function GHC$$calendar$$lastYear() {
+  return moment().hours(0).minutes(0).seconds(0).year(moment().year()-1).dayOfYear(1).local().
+      toISOString();
+}
+
 exports.short = GHC$$calendar$$short;
 exports.shortWithTime = GHC$$calendar$$shortWithTime;
+exports.now = GHC$$calendar$$now;
+exports.today = GHC$$calendar$$today;
+exports.yesterday = GHC$$calendar$$yesterday;
+exports.lastWeek = GHC$$calendar$$lastWeek;
+exports.lastMonth = GHC$$calendar$$lastMonth;
+exports.lastYear = GHC$$calendar$$lastYear;
